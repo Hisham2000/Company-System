9a9b11b93db188d1cd5750b51dd0d12df0142caf
Hisham_Anwar <hishamanwar72@gmail.com>
2022-12-04 11:19:43 +0200

    MVC Stracture

---
+++ firstTime/src/App/Controllers/Auth/LoginController.java
@@ -0,0 +1,28 @@
+
+package App.Controllers.Auth;
+
+import java.util.ArrayList;
+
+public class LoginController {
+    
+    
+    // All usert Loged in Data
+    public ArrayList<String> Auth;
+    
+    
+    
+    public LoginController()
+    {
+        // return login view
+    }
+    
+    public void check(String email, String password)
+    {
+        // make check on the user
+    }
+    
+    private void attempt()
+    {
+        // put the loged in user data in an Auth array list
+    }
+}
--- firstTime/src/Main/MainClass.java
+++ firstTime/src/Main/MainClass.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package firsttime;
+package Main;
 
 import Database.config;
 /**
  *
  * @author EtchEGo
  */
-public class FirstTime {
+public class MainClass {
 
     /**
      * @param args the command line arguments
